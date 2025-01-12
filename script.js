628e6a478fc8ce0f7d976d10f5bb66aed673570e
richgarcia7525 <richgarcia7525@gmail.com>
2025-01-12 14:30:21 -0600

    start of new HTML for project chnaged my mind om layout

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
       });
     });
+
+    const sections = document.querySelectorAll('section');
+
+const options = {
+  threshold: 0.3,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // Fixed 'classlist' typo
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  observer.observe(section); // Fixed 'observer' to 'observe'
+});
